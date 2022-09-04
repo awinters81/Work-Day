9a228335ab9ba43d94323eaa1f7e9c7a9af280c8
awinters81 <alexandria.winters81@gmail.com>
2022-09-04 12:11:57 -0400

    setting variables for timeblocks

--- js/script.js
+++ js/script.js
@@ -7,12 +7,26 @@ var currDate = moment().format("MMM Do YY");
 var currTime = moment().format('h:mm:ss a');
 
 //and I need to create variables that will pull from jQuery
-// I know I don't have to, but let me seee if I can display the current time...
-// It looks like the $ symbol will alow me to pull from jQuery and # will refernence the id 
+// The current time is displayed, but it's static. Since it's not a requirement, I'll come back to it later.
+// So now I have to figure out how I'm going to cycle through each time block.
+// Let me start by creating my variables...
+// It looks like the $ symbol will alow me to pull from jQuery and now . will reference the class 
 //I need to pull from in my HTML file
 
 var currDay = $("#currentDay");
 var currClock = $("#currentTime");
+
+// So I chose to reference these two class from my HTML file as they seem to be dealing with the
+// actual time blocking itself
+
+var timeBlock = $(".time-block")
+var scheduling = $(".container")
+
+// Also, from what I'm researching, I need to create an array because I need to store multiple records
+// of text, so... I think my brackets would be empty. Since I have an array, is console.log coming into play when I eventually
+// create more functions?
+
+var toDo = []
 
 // Displaying Date and Time
 
