0e879ed74e973eedd0f52c447b42ebc93b3e7a15
awinters81 <alexandria.winters81@gmail.com>
2022-09-04 11:03:00 -0400

    testing date display 2

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// So i know that I need variables for the date, and I think for the time blocks,
+// and the scheduling areas. It looks like I'm also pulling from jQuery and Moment.js
+
+//So for the date and time, that's being pulled from Moment...
+
+var currDate = moment().format("MMM Do YY");
+var currTime = moment().format('h:mm:ss a');
+
+//and I need to create variables that will pull from jQuery
+//So, let me start with the date...
+// It looks like the $ symbol will alow me to pull from jQuery and # will refernence the id 
+//I need to pull from in my HTML file
+
+var currDay = $("#currentDay")
+
+// I need to display the date, so I need to create a function that will allow me to do that?
+
+$currDay.text(currDate);
+
